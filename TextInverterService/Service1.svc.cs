360e6cd2398b59f077e8ebd1dc4d5164b3a8c3a2
akspatil <akshatha.patil.22@gmail.com>
2017-07-10 19:21:27 +0530

    initial commit

---
+++ TextInverterService/Service1.svc.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TextInverterService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public string ReverseEachWord(string text)
+        {
+            var textList = Regex.Split(text, @"([.,;\s-])");
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var textItem in textList)
+            {
+                char[] textArray = textItem.ToCharArray();
+                Array.Reverse(textArray);
+                string output = new string(textArray);
+                stringBuilder.Append(output);
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
